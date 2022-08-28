cede51869332d468f9e51376ba0a767587bffb98
Bruno Totoli <santosbruno101062@gmail.com>
2022-08-28 14:57:27 -0300

    RestTemplate changed to Feign Client

--- hr-payroll/src/main/java/com/bruno/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/bruno/hrpayroll/services/PaymentService.java
@@ -2,19 +2,19 @@ package com.bruno.hrpayroll.services;
 
 import com.bruno.hrpayroll.entities.Payment;
 import com.bruno.hrpayroll.entities.Worker;
-import lombok.AllArgsConstructor;
-import org.springframework.http.HttpMethod;
+import com.bruno.hrpayroll.feignclients.WorkerFeignClient;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class PaymentService {
 
-    private RestTemplate restTemplate;
+
+    private final WorkerFeignClient workerFeignClient;
 
     public Payment getPayment(Long workerId, int days) {
-        Worker worker = restTemplate.exchange("http://localhost:8081/workers/{id}", HttpMethod.GET, null, Worker.class, workerId).getBody();
+        Worker worker = workerFeignClient.findById(workerId).getBody();
         return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 
