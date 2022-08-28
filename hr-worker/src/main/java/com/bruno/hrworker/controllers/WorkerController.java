71eb2b4615580e75f9573a15474e45d6b342c002
Bruno Totoli <santosbruno101062@gmail.com>
2022-08-28 15:30:26 -0300

    Ribbon load balancing

--- hr-worker/src/main/java/com/bruno/hrworker/controllers/WorkerController.java
+++ hr-worker/src/main/java/com/bruno/hrworker/controllers/WorkerController.java
@@ -4,6 +4,7 @@ import com.bruno.hrworker.entities.Worker;
 import com.bruno.hrworker.services.WorkerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,8 @@ import org.springframework.web.bind.annotation.RestController;
 @Log4j2
 public class WorkerController {
 
+    private final Environment environment;
+
     private final WorkerService workerService;
 
     @GetMapping
@@ -25,6 +28,9 @@ public class WorkerController {
 
     @GetMapping("/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id) {
+
+        log.info("Port = "+environment.getProperty("local.server.port"));
+
         return ResponseEntity.ok(workerService.findById(id));
     }
 
