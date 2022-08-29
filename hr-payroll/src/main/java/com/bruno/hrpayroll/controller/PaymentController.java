015489e89794631abec515bd7a2f7e2e02b79b1a
Bruno Totoli <santosbruno101062@gmail.com>
2022-08-29 15:47:01 -0300

    Hystrix fault tolerance

--- hr-payroll/src/main/java/com/bruno/hrpayroll/controller/PaymentController.java
+++ hr-payroll/src/main/java/com/bruno/hrpayroll/controller/PaymentController.java
@@ -2,6 +2,7 @@ package com.bruno.hrpayroll.controller;
 
 import com.bruno.hrpayroll.entities.Payment;
 import com.bruno.hrpayroll.services.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,9 +17,15 @@ public class PaymentController {
 
     private final PaymentService paymentService;
 
+    @HystrixCommand(fallbackMethod = "getPaymentAlternative")
     @GetMapping("/{workerId}/days/{days}")
     public ResponseEntity<Payment> getPayment(@PathVariable long workerId, @PathVariable Integer days) {
         return ResponseEntity.ok(paymentService.getPayment(workerId, days));
+    }
+
+    public ResponseEntity<Payment> getPaymentAlternative(long workerId, Integer days) {
+        Payment payment = new Payment("Brann", 400.0, days);
+        return ResponseEntity.ok(payment);
     }
 
 }
