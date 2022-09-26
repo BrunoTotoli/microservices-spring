c046d5ad3c29e5db9bf9189149b4ae4c077ebaad
Bruno Totoli <santosbruno101062@gmail.com>
2022-09-26 15:30:37 -0300

    Zuul timeout

--- hr-payroll/src/main/java/com/bruno/hrpayroll/controller/PaymentController.java
+++ hr-payroll/src/main/java/com/bruno/hrpayroll/controller/PaymentController.java
@@ -4,6 +4,8 @@ import com.bruno.hrpayroll.entities.Payment;
 import com.bruno.hrpayroll.services.PaymentService;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,13 +15,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/payments")
 @RequiredArgsConstructor
+@Log4j2
 public class PaymentController {
+
+    private final Environment environment;
 
     private final PaymentService paymentService;
 
     @HystrixCommand(fallbackMethod = "getPaymentAlternative")
     @GetMapping("/{workerId}/days/{days}")
     public ResponseEntity<Payment> getPayment(@PathVariable long workerId, @PathVariable Integer days) {
+        log.info("Port = "+environment.getProperty("local.server.port"));
         return ResponseEntity.ok(paymentService.getPayment(workerId, days));
     }
 
